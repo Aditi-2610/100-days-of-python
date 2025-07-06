c03a3430853915d03d6c57d2f7c549cebe19ec75
$alimkarcc <aditilimkar26@gmail.com>
2025-07-06 16:39:39 -0500

    Updated root README with intro and Day 1 log

---
+++ day01/Band_name_generator.py
@@ -0,0 +1,10 @@
+# Create a greeting for your program.
+# Ask the user for the city that they grew up in and store it in a variable.
+# Ask the user for the name of a pet and store it in a variable.
+# Combine the name of their city and pet and show them their band name.
+
+print("Welcome to the Band name generator")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+band_name = city_name+" "+pet_name
+print("Your band name could be "+band_name)
---
+++ day01/practice.py
@@ -0,0 +1,9 @@
+print('Hello World!')
+# 1. Use \n to add another line of "Hello world".
+print("Hello"+" "+"Angela")
+# input() function to collect user input and use it within your code.
+print("Hello" + " "+ input("What is your name?") + "!")
+#  2. Check the length of the user input
+username = input("What is your name?")
+length = len(username)
+print(length)
